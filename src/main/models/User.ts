932be011d9b5f7b37b3c7479cd5772577975d815
David Scheiner <david.scheiner@century.tech>
2018-01-17 16:09:52 +0000

    first run -- still not working

--- src/main/models/User.ts
+++ src/main/models/User.ts
@@ -1,5 +1,5 @@
-import { User } from "pangea-models/users/User";
-import { UserInterface, ValidationResult } from "pangea-models/users/UserBase";
+import { User } from "pangea-models";
+// import { UserInterface, ValidationResult } from "pangea-models/users/UserBase";
 export class ApiUser {
 
   private model: User;
@@ -8,9 +8,9 @@ export class ApiUser {
     this.model = new User(data);
   }
 
-  validate(): Promise<UserInterface> {
+  validate(): Promise<Ctek.UserInterface> {
     return this.model.validate()
-      .then((result: ValidationResult) => {
+      .then((result: User.ValidationResult) => {
         if (result.valid) {
           return Promise.resolve(result.value);
         } else {
